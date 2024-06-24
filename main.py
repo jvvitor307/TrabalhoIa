1583554483258617dcf33b9411e6426913d1d246
jvvitor307 <jvvitor307@gmail.com>
2024-06-24 04:25:24 -0300

    bem longe

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import cv2
+from test import *
+from AlunosFace import *
+import os
+alunos = "./alunosFace/"
+
 path = "/home/jvvitor307/miniconda3/envs/TrabalhoIA/share/opencv4/haarcascades/haarcascade_frontalface_alt2.xml"
 clf = cv2.CascadeClassifier(path)
-
-
 cap = cv2.VideoCapture(0)
-while(not cv2.waitKey(20) & 0xFF == ord('q')):
+while not cv2.waitKey(20) & 0xFF == ord('q'):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = clf.detectMultiScale(gray)
+    gray = EscalaCinza(frame)
+    faces = DetectarRosto(gray)
+    recorte = cortarImagem(faces, frame)
+    try:
+        salvarImagem("saida.png", recorte)
+    except Exception as e:
+        print("Nenhum rosto detectado")
     for x, y, w, h in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0))
+
     cv2.imshow('frame', frame)
-cv2.imwrite("starry_night222.png", frame)
+    for aluno in os.listdir(alunos):
+        caminho = alunos+aluno
+        try:
+            result = verificacao("saida.png", caminho)
+            print(f"{aluno[:-4]}:{result}")
+        except Exception as e:
+            print("Nehnhum rosto detectado")
+
 cap.release()
 cv2.destroyAllWindows()
 cv2.waitKey(1)
--- test.py
+++ test.py
@@ -1,16 +1,10 @@
 from face_lib import face_lib
 import cv2
-
-
 FL = face_lib()
 
 
-
-img = cv2.imread("./starry_night222.png")
-faces = FL.get_faces(img) #return list of RGB faces image
-
-img_to_verfiy = cv2.imread("./starry_night222.png") #image that contain face you want verify
-gt_image = cv2.imread("./starry_night.png") #image of the face to compare with
-
-face_exist, no_faces_detected = FL.recognition_pipeline(img_to_verfiy, gt_image)
-print(face_exist)
+def verificacao(img_to_verify, gt_image):
+    saida = cv2.imread(img_to_verify)
+    base = cv2.imread(gt_image)
+    face_exist, no_faces_detected = FL.recognition_pipeline(saida, base, threshold = 0.95)
+    return face_exist
