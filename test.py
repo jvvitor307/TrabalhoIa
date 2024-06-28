6727d48b6c1b8ceaf33d5cb71624aa2924da0ac5
jvvitor307 <jvvitor307@gmail.com>
2024-06-28 10:43:07 -0300

    bem longe

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import keras
+import cv2
 from untitled1 import DistanceLayer
 custom_objects = {"DistanceLayer": DistanceLayer}
 
 with keras.saving.custom_object_scope(custom_objects):
-    reconstructed_model = keras.models.load_model("faceRecognition.keras")
+    reconstructed_model = keras.models.load_model("faceRecognition.tf.keras")
+
